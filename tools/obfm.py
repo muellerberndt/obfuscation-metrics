dcffa8be8bb7a5a7ce4091d1eb2a499b5f222a9e
serge-sans-paille <sguelton@quarkslab.com>
2016-12-01 16:06:37 +0100

    Improve obfm script
    - PEP8
    - BSD license
    - better argument checking

--- tools/obfm.py
+++ tools/obfm.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2016 All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 
 import bz2
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('files', metavar='input_file', type=argparse.FileType('r'), nargs='+', help='List of files')
+parser.add_argument('orig',
+                    type=argparse.FileType('r'),
+                    help='reference file')
+parser.add_argument('obf',
+                    type=argparse.FileType('r'),
+                    help='obfuscated file')
 args = parser.parse_args()
 
-bytesX, bytesY = args.files[0].read(), args.files[1].read()
+bytesX, bytesY = args.orig.read(), args.obf.read()
 
 ncBytesXY = len(bz2.compress(bytesX + bytesY))
 ncBytesX = len(bz2.compress(bytesX))
 ncBytesY = len(bz2.compress(bytesY))
 
 # see http://phrack.org/issues/68/15.html
-# The formula returns a value from returns a value from 0.0 (maximally similar) to 1.0 (maximally dissimilar).
+# The formula returns a value from returns a value from 0.0 (maximally similar)
+# to 1.0 (maximally dissimilar).
 
 print("ncBytesXY: " + str(ncBytesXY))
 print("ncBytesX: " + str(ncBytesX))
